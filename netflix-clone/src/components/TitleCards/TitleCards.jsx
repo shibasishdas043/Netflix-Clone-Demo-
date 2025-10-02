cd143284aee1bcc83f7ebf5f6ea0e34e593b8279
Shibasish <shibasishdas043@gmail.com>
2025-10-03 00:55:48 +0530

    ok

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import "./TitleCards.css"
-const TitleCards = () => {
-  return (
-    <div className='titlecards'>TitleCards</div>
-  )
-}
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-export default TitleCards
+const TitleCards = () => {
+
+  const cardsRef = useRef();
+
+  const handleWheel=(event)=>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>Popular On Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
