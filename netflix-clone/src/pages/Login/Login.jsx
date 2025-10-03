ebae11c471ce3935925c0b409775e4f9d697a51a
Shibasish Das <107395100+shibasishdas043@users.noreply.github.com>
2025-10-03 22:07:07 +0530

    ok

--- netflix-clone/src/pages/Login/Login.jsx
+++ netflix-clone/src/pages/Login/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import "./Login.css"
+import logo from "../../assets/logo.png";
+
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} className="login-logo" />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email'/>
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="from-help">
+            <div className="remeber">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help</p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
